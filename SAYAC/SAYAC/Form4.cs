ff7d9541f3d3868b21ce9f7e866ef7a3a374f12a
dadas99 <71325052+dadas99@users.noreply.github.com>
2020-09-25 22:12:08 +0300

    Add files via upload

---
+++ SAYAC/SAYAC/Form4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlTypes;
+
+namespace SAYAC
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FileStream fs = new FileStream(@"C:\yeniklasor\deneme.txt", FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write);
+            //StreamWriter sw = new StreamWriter(fs);
+
+            //sw.WriteLine(textBox1.Text);
+            //sw.WriteLine(textBox2.Text);
+
+            //sw.Close();
+            //fs.Close();
+            SqlC.Class1.dosyadanOku();
+
+
+        }
+    }
+}
---
+++ SAYAC/SAYAC/SqlC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SqlC
+
+{
+    class Class1
+    {
+        public static SqlConnection con = new SqlConnection(@"server=" + "");
+
+
+
+        public static void dosyadanOku()
+        {
+            string dosya_yolu = @"C:\yeniklasor\deneme.txt";
+           
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+         
+            StreamReader sw = new StreamReader(fs);
+            
+            string yazi = sw.ReadLine();
+            while (yazi != null)
+            {
+
+                yazi = sw.ReadLine();
+                MessageBox.Show(yazi);
+            }
+           
+
+
+            sw.Close();
+            fs.Close();
+            
+        }
+    }
+}
